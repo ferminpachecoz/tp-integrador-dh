b8584a2555f185e096e082578c6d566ddb51c72a
Mauge Casal <maugecasal@MacBook-Pro-de-Mauge.local>
2023-06-06 16:15:35 -0300

    genres

--- js/genres.js
+++ js/genres.js
@@ -11,3 +11,17 @@ formulario.addEventListener("submit", function(event){
         this.submit()
       }
 })
+
+let url = "https://api.deezer.com/genre"
+fetch(url)
+.then(function(response) {
+    return response.json()
+
+  })
+  .then(function(data) {
+    console.log(data);
+
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  })
