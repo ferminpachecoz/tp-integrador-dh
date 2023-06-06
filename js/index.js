9373ba95ebaf5f59c6270691419c50acb78348d6
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2023-06-06 10:53:14 -0300

    css card

--- js/index.js
+++ js/index.js
@@ -31,10 +31,8 @@ fetch(url)
           html = `
           <article class="card">
           <a href="./detalleartista.html?id=${artistas[i].id}">
-          <div class="card">
           <img src="${artistas[i].picture}" alt="">
           <p class="title">${artistas[i].name}</p>
-          </div>
           </a>
           </article>
           `;
@@ -62,11 +60,9 @@ fetch(url)
           htmlalbum = `
         <article class="card">
           <a href="./detallealbum.html?id=${albumes[i].id}">
-          <div class="card">
           <img src="${albumes[i].cover}" alt="">
           <p class="title">${albumes[i].title}</p>
           <p class="subtitle">${albumes[i].artist.name}</p>
-          </div>
           </a>
         </article>
           `;
@@ -94,11 +90,9 @@ fetch(url)
             htmlcanciones = `
           <article class="card">
             <a href="./datelladecancion.html?id=${canciones[i].id}">
-            <div class="card">
             <img src="${canciones[i].album.cover}" alt="">
             <p class="title">${canciones[i].title}</p>  
             <p class="subtitle">${canciones[i].artist.name}</p>
-            </div>
             </a>
           </article>
             `;
