b8351ad89f6fb55d1867f2d8af471a96b7fc8eb8
Mauge Casal <maugecasal@MacBook-Pro-de-Mauge.local>
2023-06-07 14:41:57 -0300

    arreglos y js

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -12,3 +12,30 @@ formulario.addEventListener("submit", function(event){
         this.submit()
       }
 })
+
+//detalles
+let id = new URLSearchParams(window.location.search).get("id")
+let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}`
+fetch(url)
+.then(function(response) {
+  return response.json()
+
+})
+.then(function(data) {
+  console.log(data);
+  
+      let generoContainer = document.querySelector(".data")
+      let html = ""
+    
+      
+        html = `
+        <img src="${data.picture}" alt="">
+        <p>Nombre del cantante: ${data.name}</p>
+       
+        `;
+        generoContainer.innerHTML += html
+
+    })
+.catch(function(error) {
+  console.log("Error: " + error);
+})
