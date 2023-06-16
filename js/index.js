950241480649fc8bddee5c0d10fbf857ce1059bf
Mauge Casal <maugecasal@MacBook-Pro-de-Mauge.local>
2023-06-16 09:15:55 -0300

    para que la primer busqueda no salta null

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+//busqueda
+let formulario = document.querySelector(".form")
+let busqueda = document.querySelector(".busqueda")
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+    if (busqueda.value === "") {
+        alert("El campo no puede estar vacío.");
+      } else if (busqueda.value.length < 3) {
+        alert("El término buscado debe tener al menos 3 caracteres.");
+      } else {
+        window.location.replace(`./search-results.html?q=${busqueda.value}`)
+      }
+})
+
 //artistas
 //informacion APi
 let url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart"
